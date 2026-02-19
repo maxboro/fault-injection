e04f5761d374e372266f3054034cff222e2d4ad8
Max B <maksym_borovyk@ukr.net>
2026-02-19 14:25:16 +0200

    Add disable behaviour

--- src/fault_injection/decorators.py
+++ src/fault_injection/decorators.py
@@ -3,22 +3,25 @@ from functools import wraps
 import time
 
 
-def raise_():
+def raise_(disable=False):
     def decorator(func):
         @wraps(func)
         def wrapper(*args, **kwargs):
+            if disable:
+                return func(*args, **kwargs)
             raise RuntimeError("raise_ exception is raised")
         return wrapper
     return decorator
 
 
-def raise_random(prob_of_raise=0.1):
+def raise_random(prob_of_raise=0.1, disable=False):
     def decorator(func):
         @wraps(func)
         def wrapper(*args, **kwargs):
-            rnd = random.random()
-            if rnd < prob_of_raise:
-                raise RuntimeError("random_raise exception is raised")
+            if not disable:
+                rnd = random.random()
+                if rnd < prob_of_raise:
+                    raise RuntimeError("random_raise exception is raised")
             return func(*args, **kwargs)
         return wrapper
 
@@ -28,11 +31,12 @@ def raise_random(prob_of_raise=0.1):
     return decorator
 
 
-def delay(time_s=0.1):
+def delay(time_s=0.1, disable=False):
     def decorator(func):
         @wraps(func)
         def wrapper(*args, **kwargs):
-            time.sleep(time_s)
+            if not disable:
+                time.sleep(time_s)
             return func(*args, **kwargs)
         return wrapper
 
@@ -41,13 +45,14 @@ def delay(time_s=0.1):
 
     return decorator
 
-def delay_random(max_time_s=0.1):
+def delay_random(max_time_s=0.1, disable=False):
     def decorator(func):
         @wraps(func)
         def wrapper(*args, **kwargs):
-            rnd = random.random()
-            time_s = max_time_s * rnd
-            time.sleep(time_s)
+            if not disable:
+                rnd = random.random()
+                time_s = max_time_s * rnd
+                time.sleep(time_s)
             return func(*args, **kwargs)
         return wrapper
 
@@ -56,13 +61,14 @@ def delay_random(max_time_s=0.1):
 
     return decorator
 
-def delay_random_norm(mean_time_s=0.3, std_time_s=0.1):
+def delay_random_norm(mean_time_s=0.3, std_time_s=0.1, disable=False):
     def decorator(func):
         @wraps(func)
         def wrapper(*args, **kwargs):
-            time_s = random.gauss(mean_time_s, std_time_s)
-            time_s = max(0, time_s)
-            time.sleep(time_s)
+            if not disable:
+                time_s = random.gauss(mean_time_s, std_time_s)
+                time_s = max(0, time_s)
+                time.sleep(time_s)
             return func(*args, **kwargs)
         return wrapper
 
