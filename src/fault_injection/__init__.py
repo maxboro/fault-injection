6fc512af76737af7b51267d0149568d6daaec400
Max B <maksym_borovyk@ukr.net>
2026-02-20 09:18:45 +0200

    Name all public functions in __init__

--- src/fault_injection/__init__.py
+++ src/fault_injection/__init__.py
@@ -1,2 +1,4 @@
-from .delays import *
-from .raise_exception import *
+from .delays import (delay_inline, delay, delay_random_inline, delay_random,
+    delay_random_norm_inline, delay_random_norm)
+from .raise_exception import (raise_inline, raise_, raise_at_nth_call,
+    raise_at_nth_call_inline, raise_random_inline, raise_random)
